901e4d1967bb6ec09cd37be13575b6ec3c86c82c
mhmdramadan977 <mr19mr11mr97@gmail.com>
2024-11-23 17:03:24 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve the static files from the build folder
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handle all other routes by sending back the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Set the port (can be accessed via process.env.PORT for deployment platforms like Render)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
